1c8d85559bfe2798843b70efc07874a703cc9e8b
Dan M McGinnis <mcginnis.dan@gmail.com>
2013-10-08 22:37:53 -0400

    close to finished.

--- module.c
+++ module.c
@@ -68,11 +68,11 @@ int Exec_file_in(Command *command, int position)
 		close(pipefd[0]);
 		dup2(pipefd[1], 1);
 		close(pipefd[1]);
-		FILE *input;
+		FILE *fp;
 		char *temp;
 		
-		input = fopen(command->argv[position+1], "r");
-		if (input == NULL)
+		fp = fopen(command->argv[position+1], "r");
+		if (fp == NULL)
 		{
 			return 1;
 		}
@@ -80,13 +80,13 @@ int Exec_file_in(Command *command, int position)
 		{
 			putchar(temp);
 		}
-		fclose(*temp);
+		fclose(fp);
 		return 0;
 	}
 	close(pipefd[1]);
 	dup2(pipefd[0], 0);
 	close(pipefd[0]);
-	
+	command->argv[position] = NULL;
 	execvp(command->name, command->argv);
 	return 0;
 }
@@ -95,14 +95,13 @@ int Exec_file_in(Command *command, int position)
 
 int Exec_file_out(Command *command, int position)
 {
+	FILE *fp;
 	pid_t pid;
 	int temp = dup(1);
 	char *file_name = command->argv[position+1];
-	printf("File name %s\n", file_name);
-	fp = fopen(file_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRGRP | S_IROTH);
-    if(fp < 0)    return 1;
-    
-    if (dup2(fp, 1) < 0) 
+	command->argv[position] = NULL;
+	fp = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRGRP | S_IROTH);
+    if(fp == NULL)    
     {
     	return 1;
     }
@@ -112,10 +111,11 @@ int Exec_file_out(Command *command, int position)
 		close(fp);
 		close(temp);
 		execvp(command->name, command->argv);
+		wait(NULL);
 		return 0;
 	}
 	dup2(temp, 1);
-	close(fp);
+	fclose(fp);
 	close(temp);
 	wait(NULL);
 	return 0;
@@ -124,6 +124,64 @@ int Exec_file_out(Command *command, int position)
 
 int Exec_pipe(Command *command, int position)
 {
+	
+	char *name_1;
+	char *name_2;
+	char *argv1[1024];
+	char *argv2[1024];
+	int i = 0;
+	int j = position;
+	int k = 0;
+	int pfd[2];
+	
+	for (i; i < position; i++)
+	{
+		argv1[i] = command->argv[i];
+	}
+	name_1 = strdup(command->argv[1]);
+	argv1[position] = NULL;
+	
+	for (j; j < command->argc; j++)
+	{
+		argv2[k] = command->argv[j];
+		k++;
+	}
+	name_2 = strdup(command->argv[position]);
+	argv2[command->argc] = NULL;
+	
+	if(pipe(pfd) < 0) 
+	{
+		perror("Error on creating pipes!\n");
+		exit(EXIT_FAILURE);
+    }
+    
+    pid_t pid;
+	pid = Fork(); // first command
+	if (pid == 0)
+	{
+		if (pid == 0)
+		{
+			wait(NULL);
+		}
+		else
+		{
+			close(0); 
+        	dup(pfd[0]);
+        	close(pfd[1]);
+        	execvp(name_2, argv2);
+        }
+    }
+    else
+    {
+    	close(1); 
+        dup(pfd[1]);
+ 	    close(pfd[0]);
+        execvp(name_2, argv2);
+    }
+    
+	
+	
+	
 	return 0;
 }
 
@@ -134,6 +192,10 @@ int Exec(Command *command)
 	if (pid == 0)
 	{
 		execvp(command->name, command->argv);
+	}
+	else
+	{
+		wait(NULL);
 	}
 	
 	return 0;
